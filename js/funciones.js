bb2a294f17c9223b3723e0e6c1bb87c40528c090
sebastian castro m <sebastiancast06@hotmail.com>
2020-07-06 02:03:32 -0500

    1.0.6 funcionalides

--- js/funciones.js
+++ js/funciones.js
@@ -2,6 +2,26 @@ jQuery(document).ready(function(){
    var enlance = jQuery("fa-bars").attr("href");
 });
 
-function abrirmenu(){
+jQuery(".fa-bars").click(function(e){
+   e.preventDefault();
+ abrirmenu();
+   
 
+});
+
+jQuery("header .container nav a").click(function(){
+   var enlace = jQuery(this).attr("href");
+   abrirmenu();
+
+   jQuery("html,body").animation({
+      "scrollTop" :jQuery(enlace).position().top
+
+   },500)
+
+});
+
+function abrirmenu(){
+   jQuery("header").addClass("open");
+   jQuery("header .container .fa-bars.open i").addClass("fa-times");
+   jQuery("header .container nav").addClass("open");
 }
